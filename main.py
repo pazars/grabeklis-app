43f259b02d767b737893b4ab656d4dd91d70f257
Davis Pazars <davis.pazars@protonmail.com>
2023-10-12 10:17:23 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import openai
+import streamlit as st
+
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Atslēga",
+        key="chatbot_api_key",
+        type="password",
+    )
+
+
+logo = "./assets/logo.svg"
+st.image(logo)
+
+
+st.title("💬 Visas ziņas vienuviet.")
+st.caption("🚀 Čatbots balstīts uz ChatGPT")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Kā es varu palīdzēt?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=st.session_state.messages
+    )
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
